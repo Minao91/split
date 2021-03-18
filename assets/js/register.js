77b4586ec0f72b7e80298d9977d794130e37259a
Minao <antoine1.geoffroy@epitech.zu>
2021-03-18 14:32:43 +0100

    [ADD] Ajout animation lorsqu'on arrive sur la page inscription

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,4 @@
-// Animation ":hover body" //
+// ----- Animation ":hover body" ----- //
 var button = document.getElementById('right');
 var body = document.body;
 
@@ -8,5 +8,14 @@ button.onmouseover = function(){
 
 button.onmouseout = function(){
 	body.id = '';
+}
+// ----- END ----- //
+// ----- Animation au chargement de la page ----- //
+window.onload = () => {
+    function anim_transition_1(){
+        document.getElementById("left").style.transform = "translate(0%, 0%)";
+		document.getElementById("right").style.transform = "translate(0%, 0%)";
+    }
+    anim_transition_1();
 }
 // ----- END ----- //
